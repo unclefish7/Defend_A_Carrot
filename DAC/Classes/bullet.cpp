a3316fe7515411949ea955468d8a020ee6cecefa
zzy <xiaogongfirst@outlook.com>
2024-01-02 13:26:12 +0800

    修改了毒气弹 子弹 爆炸 的获取逻辑bug

--- DAC/Classes/bullet.cpp
+++ DAC/Classes/bullet.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:038ead9d86b240cd6ec88277d86384da891648e57b247517320e6d5e50f18b18
-size 4569
+oid sha256:55b8b0446fb20483d95394e5815ec1fc51bff037d0b2bef58a8770c5df04c789
+size 4941
--- DAC/Classes/enemy.cpp
+++ DAC/Classes/enemy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd74ffba8ef9318b0caa7201d83bac9849144dfc8c2c573cbd9bee0f4b4630c2
-size 6396
+oid sha256:ad326b77e4538de5375c93f7cdb76682281056f14f4c7b583d05bbd051409ec4
+size 6550
